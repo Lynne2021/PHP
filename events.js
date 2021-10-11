e025eff4baa60e7ae52d6f19c48b6a9bae23b783
Lynne2021 <86622973+Lynne2021@users.noreply.github.com>
2021-10-10 20:21:51 -0400

    Add files via upload

---
+++ events.js
@@ -0,0 +1,24 @@
+var checklist = document.getElementById("chicklist");
+var items = checklist.querySelectorAll("li");
+var imputs = checklist.querySelectorAll("input");
+for (var i = 0; i < items.length;i++) {
+    items[i].addEventListener("click", editItem);
+    imputs[i].addEventListener("blur", updateItem);
+    imputs[i].addEventListener("keypress", itemKeypress);
+}
+function editItem() {
+    this.className = "edit";
+    var input = this.querySelector("input");
+    input.focus();
+    input.setSelectionRange(0, input.value.length);
+}
+
+function updateItem() {
+    this.previousElementSibling.innerHTML = this.value;
+    this.parentNode.className = "";
+}
+function itemKeypress(event) {
+    if (event.which === 13);
+    updateItem.call(this)
+}
+
